3f0680dbf223e4540cac37bb041b1e7445bce845
Michael Severino <mikeyseverino@gmail.com>
2022-01-17 12:12:41 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(someActivity="roller-skate") {
+    return `This Saturday, I want to ${someActivity}!`
+  }
+  
+function mondayWork(workActivity = "go to the office"){
+    return `This Monday, I will ${workActivity}.`
+}
+
+const wrapAdjective = function(style="%"){
+    return function(adjective = "a dedicated programmer") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
